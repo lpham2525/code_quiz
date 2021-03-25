fa8f3ea0b30feef79bf2a18bc77af62fbc910247
LanChi <lpham2525@gmail.com>
2021-03-25 18:25:17 -0400

    added coding questions

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 const question = document.querySelector('#question')
 const choices = Array.from(document.querySelectorAll('.choice-text'))
-const progressText = document.querySelector('#progressTExt')
+const progressText = document.querySelector('#progressText')
 const scoreText = document.querySelector('#score')
 const ProgressBarFull = document.querySelector('#progressBarFull')
 
@@ -12,41 +12,54 @@ let availableQuestions = []
 
 const questions = [
   {
-    question: 'What is 2 +2?',
-    choice1: 5,
-    choice2: 4,
-    choice3: 3,
-    choice4: 1,
+    question: 'What is "JSON parse" used for?',
+    choice1: 'To turn an object into a string',
+    choice2: 'To turn a string into an object',
+    choice3: 'To turn a string into an integer',
+    choice4: 'To turn an integer into a string',
+    choice5: 'To turn a string into a boolean',
     answer: 2
   },
   {
-    question: 'What is 3 + 1?',
-    choice1: 5,
-    choice2: 2,
-    choice3: 4,
-    choice4: 1,
+    question: 'Which of the following statements is true regarding the data type "object"?',
+    choice1: "It's used to list only numbers",
+    choice2: "It's used to list only names",
+    choice3: "It's used to store passwords",
+    choice4: "It's used to store data in local storage",
+    choice5: "It's used to list complex data sets with key-value pairs",
     answer: 3
   },
   {
-    question: 'What is 1 + 1?',
-    choice1: 5,
-    choice2: 2,
-    choice3: 4,
-    choice4: 1,
+    question: 'In JavaScript, what is meant by "hoisting"?',
+    choice1: "It's the order in which CSS links are inputted on an HTML page",
+    choice2: "It's the alphabetical order in which key value pairs appear in the console.",
+    choice3: "It's when a function is called before it is defined",
+    choice4: "It's used to copy and paste variables into local storage",
+    choice5: 'When a button is inside a form and a programmer wants to prevent the page from refreshing when the button is clicked',
     answer: 2
   },
   {
-    question: 'What is 4 + 1?',
-    choice1: 5,
-    choice2: 2,
-    choice3: 4,
-    choice4: 1,
+    question: 'Which of the following statements is false?',
+    choice1: 'Global variables take precedence over local variables.',
+    choice2: 'Id selectors override class selectors',
+    choice3: 'Backticks, unlike using single quotes or double quotes, allow for code to be written on multiple lines',
+    choice4: 'All variables in a ternary should be defined before an operation is created',
+    choice5: 'Functions should be written with const, not let.',
+    answer: 1
+  },
+  {
+    question: 'What is the only situation where a programmer would write "event.preventDefault()"?',
+    choice1: 'When a webpage needs a reset stylesheet before any other stylesheets are linked',
+    choice2: 'When an HTML needs to be tested across multiple browsers',
+    choice3: "When a programmer needs a code editor to stop autofilling text as it's being typed",
+    choice4: 'When a programmer wants a shortcut to writing ids and classes on a tag',
+    choice5: 'When a button is inside a form and a programmer wants to prevent the page from refreshing when the button is clicked',
     answer: 1
   }
 ]
 
 const SCORE_POINTS = 1
-const MAX_QUESTIONS = 4
+const MAX_QUESTIONS = 5
 
 const getNewQuestion = () => {
   if (availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
@@ -56,12 +69,13 @@ const getNewQuestion = () => {
   questionCounter++
   progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
   ProgressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`
+
   const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
   currentQuestion = availableQuestions[questionsIndex]
   question.innerText = currentQuestion.question
 
   choices.forEach(choice => {
-    const number = choice.dataset.number
+    const number = choice.dataset['number']
     choice.innerText = currentQuestion['choice' + number]
   })
 
@@ -76,7 +90,7 @@ choices.forEach(choice => {
 
     acceptingAnswers = false
     const selectedChoice = event.target
-    const selectedAnswer = selectedChoice.dataset.number
+    const selectedAnswer = selectedChoice.dataset['number']
 
     let classToApply = selectedAnswer === currentQuestion.answer ? 'correct' : 'incorrect'
 
