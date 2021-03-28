ad7bc25c3239fe0f6632266c6a59d59357e9bda6
LanChi <lpham2525@gmail.com>
2021-03-28 16:54:54 -0400

    added final score on end.html

--- end.js
+++ end.js
@@ -1,13 +1,12 @@
 const username = document.querySelector('#username')
 const saveScoreBtn = document.querySelector('#saveScoreBtn')
-const finalScore = document.querySelector('#finalScore')
+const lastScore = document.querySelector('#lastScore')
 const mostRecentScore = localStorage.getItem('mostRecentScore')
-
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 
 const MAX_HIGH_SCORES = 5
 
-finalScore.innerText = mostRecentScore
+lastScore.innerText = mostRecentScore
 
 username.addEventListener('keyup', () => {
   saveScoreBtn.disabled = !username.value
